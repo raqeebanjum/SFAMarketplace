bb2be913f1516bf21c109c35a0b64c63ed04eb00
carlengattenby <143469757+carlengattenby@users.noreply.github.com>
2024-04-07 20:52:05 -0500

    Profile Pages

--- SFAMarketplaceWEB/Pages/Account/Profile.cshtml.cs
+++ SFAMarketplaceWEB/Pages/Account/Profile.cshtml.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SFAMarketplaceWEB.Model;
 
 namespace SFAMarketplaceWEB.Pages.Account
 {
     public class ProfileModel : PageModel
     {
+        [BindProperty]
+        public UserProfile profile {  get; set; } = new UserProfile();
         public void OnGet()
         {
+            PopulateProfile();
+        }
+
+        private void PopulateProfile()
+        {
+            profile.FirstName = "John";
+            profile.LastName = "Doe";
+            profile.Email = "johndoe@gmail.com";
+            profile.LastLoginTime = DateTime.Now;
         }
     }
 }
