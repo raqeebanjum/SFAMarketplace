71b7cd2d5820f5e6815b862e02074c13f8a989de
carlengattenby <143469757+carlengattenby@users.noreply.github.com>
2024-04-14 13:32:22 -0500

    Made changes

--- SFAMarketplaceWEB/Pages/Account/Login.cshtml.cs
+++ SFAMarketplaceWEB/Pages/Account/Login.cshtml.cs
@@ -45,8 +45,7 @@ namespace SFAMarketplaceWEB.Pages.Account
         {
             using (SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString()))
             {
-                string cmdText = "SELECT PasswordHash, UserID, FirstName, Email, LastLoginTime, RoleName FROM Users " +
-                    "INNER JOIN [Role] ON Users.RoleId = [Role].RoleID WHERE Username=@username";
+                string cmdText = "SELECT PasswordHash, UserID, FirstName, Email, LastLoginTime FROM Users WHERE Username=@username";
                 SqlCommand cmd = new SqlCommand(cmdText, conn);
                 cmd.Parameters.AddWithValue("@username", LoginUser.Username);
                 conn.Open();
@@ -69,14 +68,14 @@ namespace SFAMarketplaceWEB.Pages.Account
 
                             string name = reader .GetString(2);
                             string email = reader.GetString (3);
-                            string roleName = reader.GetString(5);
+                           
 
                             
                             Claim emailClaim = new Claim(ClaimTypes.Email, email);
                             Claim nameClaim = new Claim(ClaimTypes.Name, name);
-                            Claim roleClaim = new Claim(ClaimTypes.Role, roleName);
+                            
 
-                            List<Claim> claims = new List<Claim> { emailClaim, nameClaim, roleClaim };
+                            List<Claim> claims = new List<Claim> { emailClaim, nameClaim };
                             ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
                             ClaimsPrincipal principal = new ClaimsPrincipal(identity);
