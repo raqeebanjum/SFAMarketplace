977eabf1eefe083b4ea4444a6e6548bc842b0f68
Raqeeb Anjum <rraqeebanjum04@gmail.com>
2024-04-22 22:44:53 -0700

    made it to where whenever an item is added, it belongs to that user bc of the userID, also now supports adding image URLs

--- SFAMarketplaceWEB/Pages/Account/Login.cshtml.cs
+++ SFAMarketplaceWEB/Pages/Account/Login.cshtml.cs
@@ -41,61 +41,57 @@ namespace SFAMarketplaceWEB.Pages.Account
         }
 
 
-        private bool ValidateCredentials()
+     private bool ValidateCredentials()
+{
+    using (SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString()))
+    {
+        string cmdText = "SELECT PasswordHash, UserID, FirstName, Email, LastLoginTime FROM Users WHERE Username=@username";
+        SqlCommand cmd = new SqlCommand(cmdText, conn);
+        cmd.Parameters.AddWithValue("@username", LoginUser.Username);
+        conn.Open();
+        SqlDataReader reader = cmd.ExecuteReader();
+        if (reader.HasRows)
         {
-            using (SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString()))
+            reader.Read();
+            if (reader.IsDBNull(0))
             {
-                string cmdText = "SELECT PasswordHash, UserID, FirstName, Email, LastLoginTime FROM Users WHERE Username=@username";
-                SqlCommand cmd = new SqlCommand(cmdText, conn);
-                cmd.Parameters.AddWithValue("@username", LoginUser.Username);
-                conn.Open();
-                SqlDataReader reader = cmd.ExecuteReader();
-                if (reader.HasRows)
-                {
-                    reader.Read();
-                    if (reader.IsDBNull(0))
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        string passwordHash = reader.GetString(0);
-                        if (SecurityHelper.VerifyPassword(LoginUser.Password, passwordHash))
-                        {
-                            int userID = reader.GetInt32(1);
-                            // Calling the method for updating login time here
-                            UpdateLastLoginTime(userID);
-
-                            string name = reader .GetString(2);
-                            string email = reader.GetString (3);
-                           
-
-                            
-                            Claim emailClaim = new Claim(ClaimTypes.Email, email);
-                            Claim nameClaim = new Claim(ClaimTypes.Name, name);
-                            
-
-                            List<Claim> claims = new List<Claim> { emailClaim, nameClaim };
-                            ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-
-                            ClaimsPrincipal principal = new ClaimsPrincipal(identity);
-
-                            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
-
-
-
-
-                            return true;
-                        }
-                        else
-                        {
-                            return false;
-                        }
-                    }
-                }
                 return false;
             }
+            else
+            {
+                string passwordHash = reader.GetString(0);
+                if (SecurityHelper.VerifyPassword(LoginUser.Password, passwordHash))
+                {
+                    int userID = reader.GetInt32(1);
+                    UpdateLastLoginTime(userID);
+
+                    string name = reader.GetString(2);
+                    string email = reader.GetString(3);
+                    
+                    List<Claim> claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, userID.ToString()),
+                        new Claim(ClaimTypes.Email, email),
+                        new Claim(ClaimTypes.Name, name)
+                    };
+                    
+                    ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+
+                    HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
+        return false;
+    }
+}
+
 
         private void UpdateLastLoginTime(int userID)
         {
