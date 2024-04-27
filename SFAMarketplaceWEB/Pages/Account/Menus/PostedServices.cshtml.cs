47aa7f22f3a701213efd6b2ac327075f990b3f34
carlengattenby <143469757+carlengattenby@users.noreply.github.com>
2024-04-27 11:38:33 -0500

    Added PostedServices Razor Pages, and did teh ViewService.cshtml based on ViewItemStyleSheet for now

---
+++ SFAMarketplaceWEB/Pages/Account/Menus/PostedServices.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SFAMarketplaceWEB.Pages.Account.Menus
+{
+    public class PostedServicesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
