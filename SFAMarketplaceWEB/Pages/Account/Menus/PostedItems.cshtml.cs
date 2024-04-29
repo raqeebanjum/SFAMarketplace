185737a69ebc0f9be9a39f6624aa12d86ac793a7
Raqeeb Anjum <rraqeebanjum04@gmail.com>
2024-04-29 15:56:48 -0700

    Users can now only see items posted by other users

--- SFAMarketplaceWEB/Pages/Account/Menus/PostedItems.cshtml.cs
+++ SFAMarketplaceWEB/Pages/Account/Menus/PostedItems.cshtml.cs
@@ -45,16 +45,19 @@ namespace SFAMarketplaceWEB.Pages.Account.Menus
         private void PopulateItem(int? categoryId)
         {
             items.Clear();
+            string currentUserID = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value; // Retrieve the current user's ID
 
             using (SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString()))
             {
                 string cmdText = @"
-        SELECT ItemID, UserID, ItemName, ItemDescription, ItemPrice, ItemPhotoURL1, ItemPhotoURL2, ItemPhotoURL3, CategoryID, ItemTradeStatus, DatePosted
-        FROM Item
-        WHERE (CategoryID = @CategoryId OR @CategoryId IS NULL)";
+SELECT ItemID, UserID, ItemName, ItemDescription, ItemPrice, ItemPhotoURL1, ItemPhotoURL2, ItemPhotoURL3, CategoryID, ItemTradeStatus, DatePosted
+FROM Item
+WHERE (CategoryID = @CategoryId OR @CategoryId IS NULL)
+AND UserID != @CurrentUserID"; // Add a condition to exclude items posted by the current user
 
                 SqlCommand cmd = new SqlCommand(cmdText, conn);
                 cmd.Parameters.AddWithValue("@CategoryId", categoryId ?? (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("@CurrentUserID", currentUserID); // Pass the current user's ID to the query
                 conn.Open();
                 SqlDataReader reader = cmd.ExecuteReader();
                 while (reader.Read())
@@ -75,6 +78,7 @@ namespace SFAMarketplaceWEB.Pages.Account.Menus
                 }
             }
         }
+
 
 
 
