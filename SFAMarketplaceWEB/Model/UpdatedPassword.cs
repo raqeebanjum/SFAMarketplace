d8fd0b7264f246c896387c4994dc270b33d9ac56
Oscar Vazquez <vazquezo1@jacks.sfasu.edu>
2024-04-30 19:38:14 -0700

    made edit profile page where the users can change their password after entering their old password and then confirming their new password by reentering it.

---
+++ SFAMarketplaceWEB/Model/UpdatedPassword.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SFAMarketplaceWEB.Model
+{
+    public class UpdatedPassword
+    {
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Current Password is required.")]
+        public string OldPassword { get; set; }
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "New Password is required.")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Confirm Password is required.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
