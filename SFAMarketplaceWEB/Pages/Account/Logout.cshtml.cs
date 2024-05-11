fd1fbe1daf6e2141a6f4c50e1b9e99ce92c143ff
Raqeeb Anjum <rraqeebanjum04@gmail.com>
2024-05-10 23:55:42 -0700

    Added Logout

--- SFAMarketplaceWEB/Pages/Account/Logout.cshtml.cs
+++ SFAMarketplaceWEB/Pages/Account/Logout.cshtml.cs
@@ -2,15 +2,18 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
 
 namespace SFAMarketplaceWEB.Pages.Account
 {
     public class LogoutModel : PageModel
     {
+        // Handler for HTTP GET requests
         public async Task<IActionResult> OnGetAsync()
         {
+            // Sign out the user
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return RedirectToPage("Login");
+            return RedirectToPage("Login"); // Redirect to the login page after signing out
         }
     }
 }
