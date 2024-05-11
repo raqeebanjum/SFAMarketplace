bf94ac584a209d183ef3296f4b153baef036357c
Raqeeb Anjum <rraqeebanjum04@gmail.com>
2024-05-10 23:51:05 -0700

    Added Checkout

--- SFAMarketplaceWEB/Pages/Account/Menus/Checkout.cshtml.cs
+++ SFAMarketplaceWEB/Pages/Account/Menus/Checkout.cshtml.cs
@@ -3,10 +3,13 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SFAMarketplaceWEB.Pages.Account.Menus
 {
+    // Represents the checkout page in the application
     public class CheckoutModel : PageModel
     {
+        // GET request handler for the checkout page
         public void OnGet()
         {
+            // Code to initialize page state can be added here
         }
     }
 }
